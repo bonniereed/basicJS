e5e4feff31c247e61d5883aeb0d66c78f0a2ad15
Bonnie Reed <bonbon@Bonnies-MacBook-Pro-2.local>
2020-10-01 14:10:36 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstName = prompt ("What is your first name");
+let lastName = prompt ("What is your last name");
+let userAge = prompt ("How old are you");
+
+console.log("Your full name is" + firstName + " " + lastName)
+console.log("You are" + userAge)
