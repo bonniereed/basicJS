25766a2435275e9c699b8f9a8d31936d42f20e16
Bonnie Reed <bonbon@Bonnies-MacBook-Pro-2.local>
2020-10-01 14:12:13 -0500

    adding saved changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let firstName = prompt ("What is your first name");
 let lastName = prompt ("What is your last name");
 let userAge = prompt ("How old are you");
+let fullName = firstName + " " + lastName; 
 
-console.log("Your full name is" + firstName + " " + lastName)
-console.log("You are" + userAge)
+console.log("Your full name is " + fullName)
+console.log("You are " + userAge)
